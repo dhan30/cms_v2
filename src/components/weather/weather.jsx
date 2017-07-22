6b78a5c1891945abc33fcf1ab2940b18ab9ae390
dhan30 <hhan0524@gmail.com>
2017-07-21 21:10:57 -0400

    weather api update

--- src/components/weather/weather.jsx
+++ src/components/weather/weather.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import style from './style.css';
 
-
-
-const url = "http://api.openweathermap.org/data/2.5/weather?q=Fairfax,us&units=imperial&APPID=fa408109b6ec2e010a72ee13665a44df";
-
-
+const url = "http://api.openweathermap.org/data/2.5/weather?q=chantilly,us&units=imperial&APPID=fa408109b6ec2e010a72ee13665a44df";
 
 class Weather extends React.Component {
   constructor(props) {
@@ -16,7 +12,7 @@ class Weather extends React.Component {
   }
   componentDidMount () {
     this.weatherApi(url);
-    this.myFunc();
+
   };
 
   weatherApi() {
@@ -27,40 +23,39 @@ class Weather extends React.Component {
       this.setState({
         weatherTemp: data.main.temp,
         weatherDataP: data.main.humidity,
-        allData: data
+        allData: data,
+        name: data.name,
+        description: data.weather[0].description,
+        country: data.sys.country
       });
     }).catch((err) => {
       return err;
     }).then();
   }
-
-  myFunc () {
-    return "I am myFunc";
-    console.log("i am func");
-  }
-
-
-
   render() {
     const result = JSON.stringify(this.state.weatherTemp);
     const temp = JSON.stringify(this.state.weatherDataP);
     const data = JSON.stringify(this.state.allData);
+    const name = JSON.stringify(this.state.name);
+    const country = JSON.stringify(this.state.country);
+    const description = JSON.stringify(this.state.description);
 
     return (
       <div className={style.weatherBox}>
         <div className="container">
           <div className="row group-form">
-            <input type="text" name="city" id="city" className={style.form} placeholder="type here"></input>
-            <button id="submitWeather" className={style.btn} onClick={this.myFunc}>Hello</button>
+            <input type="text" name="city" id="city" className={style.form} placeholder="insert city"></input>
+            <button id="submitWeather" className={style.btn}>Click</button>
           </div>
         </div>
         <div>
           <p className={style.weatherTempBox}>
+            {"City:" + " " + name }<br/><br/>
+            {"Country:" + " " + country }<br/><br/>
             {"Tempature:" + " " + result + "°"}<br/><br/>
+            {"Description:" + " " + description}<br/><br/>
             {"Humidity:" + " " + temp + "%"}
-            {data}
           </p>
-
         </div>
       </div>
     );
